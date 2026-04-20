03ec1eadbed98fbcaa51c9692dc58c418055e2d1
s4nskrut1 <sanskrutigorle1@gmail.com>
2026-04-21 00:32:07 +0530

    Create book.java

---
+++ library project/book.java
@@ -0,0 +1,25 @@
+
+public class Book {
+	private String title;
+    private String author;
+
+    // Constructor
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    // Getter methods
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    // Display method
+    public void displayBook() {
+        System.out.println("Title: " + title + ", Author: " + author);
+    }
+}
